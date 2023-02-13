549f368ea1e1015669fffd6787b01d01b5d91c39
yang <jzy991126@outlook.com>
2023-02-13 15:27:04 +0800

    add argparser

--- src/phoenix/main.cpp
+++ src/phoenix/main.cpp
@@ -1,14 +1,23 @@
 #include "phoenix/utils/image_tool_class.h"
 #include "phoenix/core/parser_class.h"
 #include "phoenix/core/denoiser_class.h"
+#include "ext/argparser.hpp"
+
 using namespace std;
 using namespace Phoenix;
 
-int main() {
+int main(int argc, char const *argv[]) {
+
+    argparse::ArgumentParser program("phoenix-nirvana");
+    program.add_argument<std::string>("filepath")
+            .help("json file path");
+    program.parse_args(argc, argv);
+
+    auto filename = program.get<string>("filepath");
 
     Renderer render;
     Parser parser;
-    parser.Parse("scene/cbox/cbox.json",render);
+    parser.Parse(filename, render);
 
     render.SetMultiThread(true);
     render.Render();
@@ -18,13 +27,13 @@ int main() {
     auto normal = render.bitmap(ResType::Normal)->ConvertTo3f();
     auto albedo = render.bitmap(ResType::Albedo)->ConvertTo3f();
 
-    img_tool.write_exr(color,parser.file_name().append(".exr"));
-    img_tool.write_exr(normal,parser.file_name().append("_normal.exr"));
-    img_tool.write_exr(albedo,parser.file_name().append("_albedo.exr"));
+    img_tool.write_exr(color, parser.file_name().append(".exr"));
+    img_tool.write_exr(normal, parser.file_name().append("_normal.exr"));
+    img_tool.write_exr(albedo, parser.file_name().append("_albedo.exr"));
 
     Denoiser denoiser;
-    auto res =  denoiser.Denoise(color,albedo,normal);
-    img_tool.write_exr(res,parser.file_name().append("_denoised.exr"));
+    auto res = denoiser.Denoise(color, albedo, normal);
+    img_tool.write_exr(res, parser.file_name().append("_denoised.exr"));
 
 
 }
